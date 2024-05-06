5ab55749ca7e693f68eaaf929adeb7174c330909
jwav <wavresky.julien@gmail.com>
2024-05-06 23:13:38 +0200

    added todos for the networking modules. let's go UDP, screw TCP

--- cube_messages.py
+++ cube_messages.py
@@ -4,9 +4,13 @@ import cube_identification as cubeid
 
 import enum
 
+
 class CubeMsgType(enum.Enum):
     """Enumeration of the different types of messages that can be sent."""
     TEST = 0
+
+    GENERAL_VERSION_INFO = 1
+    GENERAL_HEARTBEAT = 2
     # Cubebox messages
     CUBEBOX_RFID_READ = 100
     CUBEBOX_BUTTON_PRESS = 101
@@ -20,17 +24,20 @@ class CubeMsgType(enum.Enum):
     FRONTDESK_NEW_TEAM = 300
 
 
+# TODO: make acknowledgement messages
+
+
 class CubeMessage:
     """Base class for all messages."""
 
-    def __init__(self, msgtype:CubeMsgType, sender:str, **kwargs):
+    def __init__(self, msgtype: CubeMsgType, sender: str, **kwargs):
         self.msgtype = msgtype
         self.sender = sender
         self.kwargs = kwargs
 
     def to_string(self):
         sep = "|"
-        return f"CUBEMSG{sep}sender={self.sender}{sep}msgtype={self.msgtype.name}{sep}{sep.join([f'{k}={v}' for k,v in self.kwargs.items()])}"
+        return f"CUBEMSG{sep}sender={self.sender}{sep}msgtype={self.msgtype.name}{sep}{sep.join([f'{k}={v}' for k, v in self.kwargs.items()])}"
 
     def __str__(self):
         return self.to_string()
@@ -38,25 +45,31 @@ class CubeMessage:
     def __repr__(self):
         return self.to_string()
 
+
 class CubeMsgNewTeam(CubeMessage):
     def __init__(self, sender, team, max_time_sec):
         super().__init__(CubeMsgType.FRONTDESK_NEW_TEAM, sender, team=team, max_time_sec=max_time_sec)
+
 
 class CubeMsgRfidRead(CubeMessage):
     def __init__(self, sender, uid):
         super().__init__(CubeMsgType.CUBEBOX_RFID_READ, sender, uid=uid)
 
+
 class CubeMsgButtonPress(CubeMessage):
     def __init__(self, sender):
         super().__init__(CubeMsgType.CUBEBOX_BUTTON_PRESS, sender)
+
 
 class CubeMsgScoresheet(CubeMessage):
     def __init__(self, sender, scoresheet):
         super().__init__(CubeMsgType.CUBEBOX_BUTTON_PRESS, sender, scoresheet=scoresheet)
 
+
 class CubeMsgTimeIsUp(CubeMessage):
     def __init__(self, sender, team):
         super().__init__(CubeMsgType.CUBESERVER_TIME_IS_UP, sender, team=team)
+
 
 if __name__ == "__main__":
     # test the CubeMessage class and all its subclasses
@@ -65,5 +78,3 @@ if __name__ == "__main__":
     print(CubeMsgRfidRead(sender_name, "1234567890"))
     print(CubeMsgButtonPress(sender_name))
     print(CubeMsgNewTeam(sender_name, "Team1", 1200))
-
-
