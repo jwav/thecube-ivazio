63b412754b06beb9a2e007e868cb7460c4fe9e9d
jwav <wavresky.julien@gmail.com>
2024-05-27 14:04:17 +0200

    made a mixin (partial class) for the "new team" tab

--- thecubeivazio/cubegui/cubegui.py
+++ thecubeivazio/cubegui/cubegui.py
@@ -10,13 +10,14 @@ from cubegui_ui import Ui_Form
 
 from thecubeivazio import cubeserver_frontdesk as cfd, cube_game, cube_utils
 from thecubeivazio import cube_logger as cube_logger
+from thecubeivazio.cubegui.cubegui_tab_newteam import CubeGuiTabNewTeamMixin
 
 import sys
 import atexit
 import traceback as tb
 
 
-class CubeGuiForm(QMainWindow):
+class CubeGuiForm(QMainWindow, CubeGuiTabNewTeamMixin):
     def __init__(self):
         super().__init__()
         self.ui = Ui_Form()
@@ -41,45 +42,15 @@ class CubeGuiForm(QMainWindow):
 
     def initial_setup(self):
 
-        self.setup_team_creation_tab()
+        self.setup_newteam_tab()
         self.setup_team_management_tab()
         self.setup_admin_tab()
 
         return True
 
-    def setup_team_creation_tab(self):
-        """Sets up the widgets of the tab 'Créer une nouvelle équipe'"""
-
-        # fill the team names combo box
-        team_names = self.fd.config.team_names
-        self.ui.comboNewteamTeamName.clear()
-        self.ui.comboNewteamTeamName.addItems(team_names)
-
-        # fill the game durations combo box
-        game_durations_str = self.fd.config.game_durations_str
-        self.ui.comboNewteamDuration.clear()
-        self.ui.comboNewteamDuration.addItems(game_durations_str)
-
-        # set up the buttons
-        self.ui.btnIconNewteamNewTeamStatus.setIcon(QtGui.QIcon())
-        self.ui.btnNewteamNewTeam.clicked.connect(self.create_new_team)
-        self.ui.btnNewteamRfidClear.clicked.connect(
-            lambda: (self.ui.lineNewteamRfid.clear(), self.log.info("RFID cleared")))
-
-        # set up the status label
-        self.update_new_team_status_label("", None)
 
 
-    def update_new_team_status_label(self, text: str, icon_name: str=None):
-        try:
-            self.ui.lblNewteamNewTeamStatusText.setText(text)
-            if icon_name:
-                self.ui.btnIconNewteamNewTeamStatus.setIcon(QtGui.QIcon.fromTheme(icon_name))
-            QApplication.processEvents()
-        except Exception as e:
-            self.log.error(f"Error in update_new_team_status_label: {e}")
-            self.ui.btnIconNewteamNewTeamStatus.setIcon(QtGui.QIcon.fromTheme("error"))
-            self.ui.lblNewteamNewTeamStatusText.setText("{e}")
+
 
     def update_team_creation_tab(self):
         raise NotImplementedError
@@ -90,24 +61,6 @@ class CubeGuiForm(QMainWindow):
     def setup_admin_tab(self):
         pass
 
-    def create_new_team(self):
-        team_name = self.ui.comboNewteamTeamName.currentText()
-        team_custom_name = self.ui.lineNewteamTeamCustomName.text()
-        rfid = self.ui.lineNewteamRfid.text()
-        allocated_time = cube_utils.hhmmss_string_to_seconds(self.ui.comboNewteamDuration.currentText())
-        if any([not team_name, not rfid, not allocated_time]):
-            self.update_new_team_status_label(f"Informations manquantes ou erronées", "error")
-            self.log.error("Missing information to create a new team.")
-            return
-        team = cube_game.CubeTeamStatus(team_name, rfid, allocated_time)
-        self.log.info(f"Creating new team: {team.to_string()}")
-        self.update_new_team_status_label(f"Création de l'équipe {team_name} en cours...", "hourglass")
-        # send the new team creation message to the cubemaster and check that the ack is ok
-        report = self.fd.add_new_team(team)
-        if report.ok:
-            self.update_new_team_status_label(f"Équipe {team_name} créée avec succès.", "ok")
-        else:
-            self.update_new_team_status_label(f"Échec de la création de l'équipe {team_name} : {report.ack_info}", "error")
 
     def closeEvent(self, event):
         self.log.info("Closing CubeGui...")
---
+++ thecubeivazio/cubegui/cubegui_tab_newteam.py
@@ -0,0 +1,79 @@
+"""File for the mixin class (partial for CubeGuiForm) for the new team tab."""
+
+import threading
+import time
+
+from PyQt5 import QtGui
+from PyQt5.QtCore import QFile, QTextStream
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from cubegui_ui import Ui_Form
+
+from thecubeivazio import cubeserver_frontdesk as cfd, cube_game, cube_utils
+from thecubeivazio import cube_logger as cube_logger
+
+import sys
+import atexit
+import traceback as tb
+
+
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from cubegui import CubeGuiForm
+
+class CubeGuiTabNewTeamMixin:
+    def setup_newteam_tab(self: 'CubeGuiForm'):
+        """Sets up the widgets of the tab 'Créer une nouvelle équipe'"""
+
+        # fill the team names combo box
+        team_names = self.fd.config.team_names
+        self.ui.comboNewteamTeamName.clear()
+        self.ui.comboNewteamTeamName.addItems(team_names)
+
+        # clear the RFID line edit
+        self.ui.lineNewteamRfid.clear()
+
+        # fill the game durations combo box
+        game_durations_str = self.fd.config.game_durations_str
+        self.ui.comboNewteamDuration.clear()
+        self.ui.comboNewteamDuration.addItems(game_durations_str)
+
+        # set up the buttons
+        self.ui.btnIconNewteamNewTeamStatus.setIcon(QtGui.QIcon())
+        self.ui.btnNewteamNewTeam.clicked.connect(self.create_new_team)
+        self.ui.btnNewteamRfidClear.clicked.connect(
+            lambda: (self.ui.lineNewteamRfid.clear(), self.log.info("RFID cleared")))
+
+        # set up the status label
+        self.update_new_team_status_label("", None)
+
+
+    def update_new_team_status_label(self:'CubeGuiForm', text: str, icon_name: str=None):
+        try:
+            self.ui.lblNewteamNewTeamStatusText.setText(text)
+            if icon_name:
+                self.ui.btnIconNewteamNewTeamStatus.setIcon(QtGui.QIcon.fromTheme(icon_name))
+            QApplication.processEvents()
+        except Exception as e:
+            self.log.error(f"Error in update_new_team_status_label: {e}")
+            self.ui.btnIconNewteamNewTeamStatus.setIcon(QtGui.QIcon.fromTheme("error"))
+            self.ui.lblNewteamNewTeamStatusText.setText("{e}")
+
+    def create_new_team(self: 'CubeGuiForm'):
+        team_name = self.ui.comboNewteamTeamName.currentText()
+        team_custom_name = self.ui.lineNewteamTeamCustomName.text()
+        rfid = self.ui.lineNewteamRfid.text()
+        allocated_time = cube_utils.hhmmss_string_to_seconds(self.ui.comboNewteamDuration.currentText())
+        if any([not team_name, not rfid, not allocated_time]):
+            self.update_new_team_status_label(f"Informations manquantes ou erronées", "error")
+            self.log.error("Missing information to create a new team.")
+            return
+        team = cube_game.CubeTeamStatus(team_name, rfid, allocated_time)
+        self.log.info(f"Creating new team: {team.to_string()}")
+        self.update_new_team_status_label(f"Création de l'équipe {team_name} en cours...", "hourglass")
+        # send the new team creation message to the cubemaster and check that the ack is ok
+        report = self.fd.add_new_team(team)
+        if report.ok:
+            self.update_new_team_status_label(f"Équipe {team_name} créée avec succès.", "ok")
+        else:
+            self.update_new_team_status_label(f"Échec de la création de l'équipe {team_name} : {report.ack_info}", "error")
