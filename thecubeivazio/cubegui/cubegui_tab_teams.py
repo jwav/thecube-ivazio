70e71654392bb0f0fef09db2c3fe1e8b23c2b027
jwav <wavresky.julien@gmail.com>
2024-05-27 20:15:59 +0200

    finished implementing teams table. need to fix something concerning trophies, though. they are not displayed

--- thecubeivazio/cubegui/cubegui_tab_teams.py
+++ thecubeivazio/cubegui/cubegui_tab_teams.py
@@ -27,7 +27,7 @@ class CubeGuiTabTeamsMixin:
         self.ui: Ui_Form
 
         # fill the team names combo box
-        team_names = [""] # empty string for "all teams"
+        team_names = [""]  # empty string for "all teams"
         team_names.extend(self.fd.config.team_names)
         print(team_names)
         self.ui.comboTeamsTeamName.clear()
@@ -41,12 +41,6 @@ class CubeGuiTabTeamsMixin:
 
         # connect the "search" button
         self.ui.btnTeamsSearch.clicked.connect(self.search_teams)
-
-        # clear the search results list
-        self.ui.listTeamsSearchResults.clear()
-
-        # clear the team info table
-        self.ui.tableTeamsTeamInfo.clearContents()
 
         # fill the trophy combo box
         trophies = self.fd.config.trophies
@@ -66,26 +60,29 @@ class CubeGuiTabTeamsMixin:
         self.ui.btnTeamsPrintScoresheet.clicked.connect(self.print_scoresheet)
 
         # update the tab
-        self.update_teams_info_table()
+        self.update_tab_teams()
+
+    def update_tab_teams(self: 'CubeGuiForm'):
+        pass
 
     def print_scoresheet(self: 'CubeGuiForm'):
         pass
+
     def add_trophy(self: 'CubeGuiForm'):
         pass
+
     def remove_trophy(self: 'CubeGuiForm'):
         pass
-
 
     def search_teams(self: 'CubeGuiForm'):
         self.ui: Ui_Form
 
         team_name = self.ui.comboTeamsTeamName.currentText()
-        custom_name:str = self.ui.lineTeamsCustomName.text()
+        custom_name: str = self.ui.lineTeamsCustomName.text()
         rfid_uid = self.ui.lineTeamsRfid.text()
         search_currently_playing = self.ui.radioTeamsCurrentlyPlaying.isChecked()
         self.log.info(f"Searching teams matching: name={team_name}, custom_name={custom_name}, "
                       f"rfid={rfid_uid}, currently_playing={search_currently_playing}")
-
 
         # get the search parameters
 
@@ -119,99 +116,35 @@ class CubeGuiTabTeamsMixin:
                 continue
             matching_teams.append(team)
 
-        # display the search results in the list
-        # Format : name - custom_name : french_dat : hhhmm_start -> hhmm_end : points
-        self.ui.listTeamsSearchResults.clear()
+        self.ui.tableTeamsResults.clearContents()
+        self.ui.tableTeamsResults.setColumnCount(9)
+        self.ui.tableTeamsResults.setHorizontalHeaderLabels(
+            ["Date", "Nom", "Nom personnalisé", "Score", "Cubes faits", "Trophées", "Début", "Fin", "RFID"])
+        self.ui.tableTeamsResults.setRowCount(len(matching_teams))
 
-        for team in matching_teams:
+        for i, team in enumerate(matching_teams):
             french_date = cube_utils.timestamp_to_french_date(team.start_timestamp)
-            start_tod = cube_utils.timestamp_to_hhmmss_time_of_day_string(team.start_timestamp, separators=":", secs=False)
-            end_tod = cube_utils.timestamp_to_hhmmss_time_of_day_string(team.end_timestamp, separators=":", secs=False)
-            self.ui.listTeamsSearchResults.addItem(
-                f"{team.name} - {team.custom_name} : {french_date} : "
-                f"{start_tod} -> {start_tod} : {team.calculate_score()} points")
+            start_tod = cube_utils.timestamp_to_hhmmss_time_of_day_string(
+                team.start_timestamp, separators=":", secs=False)
+            end_tod = cube_utils.timestamp_to_hhmmss_time_of_day_string(
+                team.end_timestamp, separators=":", secs=False)
+            trophies_names = [t.name for t in team.trophies]
+            print(f"team: {team}")
+            self.ui.tableTeamsResults.setItem(i, 0, QTableWidgetItem(french_date))
+            self.ui.tableTeamsResults.setItem(i, 1, QTableWidgetItem(team.name))
+            self.ui.tableTeamsResults.setItem(i, 2, QTableWidgetItem(team.custom_name))
+            self.ui.tableTeamsResults.setItem(i, 3, QTableWidgetItem(str(team.calculate_score())))
+            self.ui.tableTeamsResults.setItem(i, 4, QTableWidgetItem(str(team.completed_cubebox_ids)))
+            self.ui.tableTeamsResults.setItem(i, 5, QTableWidgetItem(", ".join(trophies_names)))
+            self.ui.tableTeamsResults.setItem(i, 6, QTableWidgetItem(start_tod))
+            self.ui.tableTeamsResults.setItem(i, 7, QTableWidgetItem(end_tod))
+            self.ui.tableTeamsResults.setItem(i, 8, QTableWidgetItem(team.rfid_uid))
+        # Resize columns to fit contents and headers
+        self.ui.tableTeamsResults.resizeColumnsToContents()
 
-        # if there are results, select the first one
-        if matching_teams:
-            self.ui.listTeamsSearchResults.setCurrentRow(0)
-            self.ui.tableTeamsResults.clearContents()
-            self.ui.tableTeamsResults.setColumnCount(9)
-            # columns : date, name, custom_name, score, completed cubes, trophies, start time, end time, rfid uid
-            self.ui.tableTeamsResults.setHorizontalHeaderLabels([
-                "Date", "Nom", "Nom personnalisé", "Score", "Cubes faits", "Trophées", "Début", "Fin", "RFID UID"])
-
-
-            self.ui.tableTeamsResults.setRowCount(len(matching_teams))
-
-            for i, team in enumerate(matching_teams):
-                french_date = cube_utils.timestamp_to_french_date(team.start_timestamp)
-                start_tod = cube_utils.timestamp_to_hhmmss_time_of_day_string(team.start_timestamp, separators=":", secs=False)
-                end_tod = cube_utils.timestamp_to_hhmmss_time_of_day_string(team.end_timestamp, separators=":", secs=False)
-                trophies_names = [t.name for t in team.trophies]
-                self.ui.tableTeamsResults.setItem(i, 0, QTableWidgetItem(french_date))
-                self.ui.tableTeamsResults.setItem(i, 1, QTableWidgetItem(team.name))
-                self.ui.tableTeamsResults.setItem(i, 2, QTableWidgetItem(team.custom_name))
-                self.ui.tableTeamsResults.setItem(i, 3, QTableWidgetItem(str(team.calculate_score())))
-                self.ui.tableTeamsResults.setItem(i, 4, QTableWidgetItem(str(team.completed_cubebox_ids)))
-                self.ui.tableTeamsResults.setItem(i, 5, QTableWidgetItem(", ".join(trophies_names)))
-                self.ui.tableTeamsResults.setItem(i, 6, QTableWidgetItem(start_tod))
-                self.ui.tableTeamsResults.setItem(i, 7, QTableWidgetItem(end_tod))
-                self.ui.tableTeamsResults.setItem(i, 8, QTableWidgetItem(team.rfid_uid))
-
-
-
-
-        # update this tab so as to display the selected team's info in the table
-        self.update_teams_info_table()
-
-
-    def update_teams_info_table(self: 'CubeGuiForm'):
-        # get the selected team in the list
-        selected_row = self.ui.listTeamsSearchResults.currentRow()
-        print(f"selected_row: {selected_row}")
-        if selected_row < 0 or selected_row >= len(self.fd.teams):
-            return
-        team = self.fd.teams[selected_row]
-
-        # fill the team info table. Rows:
-        # - Nom
-        # - Nom personnalisé
-        # - Date
-        # - Score
-        # - Cubes faits
-        # - Trophées
-        # - Durée
-        # - Heure début
-        # - Heure fin
-        # - RFID UID
-        self.ui.tableTeamsTeamInfo.clearContents()
-        self.ui.tableTeamsTeamInfo.setRowCount(10)
-        self.ui.tableTeamsTeamInfo.setColumnCount(2)
-
-        french_date = cube_utils.timestamp_to_french_date(team.start_timestamp)
-        start_tod = cube_utils.timestamp_to_hhmmss_time_of_day_string(team.start_timestamp, separators=":", secs=False)
-        end_tod = cube_utils.timestamp_to_hhmmss_time_of_day_string(team.end_timestamp, separators=":", secs=False)
-        trophies_names = [t.name for t in team.trophies]
-
-        self.ui.tableTeamsTeamInfo.setItem(0, 0, QTableWidgetItem("Nom"))
-        self.ui.tableTeamsTeamInfo.setItem(0, 1, QTableWidgetItem(team.name))
-        self.ui.tableTeamsTeamInfo.setItem(1, 0, QTableWidgetItem("Nom personnalisé"))
-        self.ui.tableTeamsTeamInfo.setItem(1, 1, QTableWidgetItem(team.custom_name))
-        self.ui.tableTeamsTeamInfo.setItem(2, 0, QTableWidgetItem("Date"))
-        self.ui.tableTeamsTeamInfo.setItem(2, 1, QTableWidgetItem(french_date))
-        self.ui.tableTeamsTeamInfo.setItem(3, 0, QTableWidgetItem("Score"))
-        self.ui.tableTeamsTeamInfo.setItem(3, 1, QTableWidgetItem(str(team.calculate_score())))
-        self.ui.tableTeamsTeamInfo.setItem(4, 0, QTableWidgetItem("Durée"))
-        self.ui.tableTeamsTeamInfo.setItem(4, 1, QTableWidgetItem(cube_utils.seconds_to_hhmmss_string(team.allocated_time)))
-        self.ui.tableTeamsTeamInfo.setItem(5, 0, QTableWidgetItem("Cubes faits"))
-        self.ui.tableTeamsTeamInfo.setItem(5, 1, QTableWidgetItem(str(team.completed_cubebox_ids)))
-        self.ui.tableTeamsTeamInfo.setItem(6, 0, QTableWidgetItem("Trophées"))
-        self.ui.tableTeamsTeamInfo.setItem(6, 1, QTableWidgetItem(", ".join(trophies_names)))
-        self.ui.tableTeamsTeamInfo.setItem(7, 0, QTableWidgetItem("Heure début"))
-        self.ui.tableTeamsTeamInfo.setItem(7, 1, QTableWidgetItem(start_tod))
-        self.ui.tableTeamsTeamInfo.setItem(8, 0, QTableWidgetItem("Heure fin"))
-        self.ui.tableTeamsTeamInfo.setItem(8, 1, QTableWidgetItem(end_tod))
-        self.ui.tableTeamsTeamInfo.setItem(9, 0, QTableWidgetItem("RFID UID"))
-        self.ui.tableTeamsTeamInfo.setItem(9, 1, QTableWidgetItem(team.rfid_uid))
-
-        QApplication.processEvents()
+if __name__ == "__main__":
+    from cubegui import CubeGuiForm
+    app = QApplication(sys.argv)
+    window = CubeGuiForm()
+    window.show()
+    sys.exit(app.exec_())
