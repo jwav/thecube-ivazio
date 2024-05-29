ff5d5a52c7adbb106d7b4076f016563bea66721e
jwav <wavresky.julien@gmail.com>
2024-05-29 22:40:05 +0200

    setting up the cubemaster's linux

--- install_required_apt_packages.sh
+++ install_required_apt_packages.sh
@@ -6,7 +6,7 @@ sudo apt install make build-essential libssl-dev zlib1g-dev libbz2-dev libreadli
 sudo apt-get install software-properties-common
 sudo apt-get install python3-pip
 sudo apt-get install python3-venv
-sudo apt-get install python3-is-python
+sudo apt-get install python-is-python3
 sudo apt-get install git
 sudo apt-get install xvfb
 sudo apt-get install x11-utils
