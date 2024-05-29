3c5ac0f6132e42f015b638a3ed39704ce4f4cbb2
jwav <wavresky.julien@gmail.com>
2024-05-29 12:47:31 +0200

    bettered the updating system

--- update_version.sh
+++ update_version.sh
@@ -1,4 +1,25 @@
 #!/usr/bin/bash
-source ./install_required_apt_packages.sh
-git pull && pip install -r ./requirements.txt && python3 install .
+
+SKIP_APT=false
+for arg in "$@"
+do
+  if [ "$arg" == "--skip-apt" ]; then
+    SKIP_APT=true
+    break
+  fi
+done
+
+if [ "$SKIP_APT" = false ]; then
+./install_required_apt_packages.sh
+if [ $? -ne 0 ]; then
+    echo "ERROR: APT udpate and install failed"
+    exit 1
+  else
+    echo "OK : The script succeeded"
+fi
+
+git pull
+pip install -r ./requirements.txt
+python3 install .
+
 echo "Update OK: APT packages installed, git pulled, project package pip installed."
