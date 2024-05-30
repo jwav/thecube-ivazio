05ce5a3e9529a15d1e5cb96081b2aa7051279b42
jwav <wavresky.julien@gmail.com>
2024-05-30 16:47:16 +0200

    updated test.sh for my_rgb

--- thecubeivazio/my_rgb_samples/test.sh
+++ thecubeivazio/my_rgb_samples/test.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # if i omit the led-chain arg, it displays the same image on all panels
-sudo ./my_display_text.py --led-cols=64 --led-rows=32 --led-chain=2 --led-slowdown=5
-#sudo ./my_display_text_not_working.py --led-cols=64 --led-rows=32 --led-chain=2
-#sudo ./my_display_text.py --led-cols=64 --led-rows=32 --led-chain=1
+python3 ./my_display_text.py --led-cols=64 --led-rows=32 --led-chain=2 --led-slowdown=5
+#python3 ./my_display_text_not_working.py --led-cols=64 --led-rows=32 --led-chain=2
+#python3 ./my_display_text.py --led-cols=64 --led-rows=32 --led-chain=1
 
