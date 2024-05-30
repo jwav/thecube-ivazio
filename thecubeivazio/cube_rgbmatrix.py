b62b45184cc585a5d69e1f5c4dde7183bb814e10
jwav <wavresky.julien@gmail.com>
2024-05-30 18:06:05 +0200

    trying to make my own matrix text displayer based on the sample from the lib

--- thecubeivazio/cube_rgbmatrix.py
+++ thecubeivazio/cube_rgbmatrix.py
@@ -12,86 +12,75 @@ import datetime
 from thecubeivazio.cube_common_defines import *
 from thecubeivazio import cube_utils
 
+NB_MATRICES = 2
+PANEL_WIDTH = 64
+PANEL_HEIGHT = 32
+X_MARGIN = 0
+Y_TEXT = 30
+LED_SLOWDOWN_GPIO = 5
 
 class CubeRgbTextDrawer(SampleBase):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        known_args, _ = self.parser.parse_known_args([
+            f'--led-cols={PANEL_WIDTH}',
+            f'--led-rows={PANEL_HEIGHT}',
+            f'--led-chain={NB_MATRICES}',
+            f'--led-slowdown-gpio={LED_SLOWDOWN_GPIO}'
+        ])
+        self.args = known_args
+        self._thread = threading.Thread()
+        self._keep_running = False
+        self.messages = []
 
     def run(self):
+        # NOTE: DO NOT copy the canvas in a CubeRgbText instance property.
+        # it seems to create new canvas instances, and makes the message disappear
         offscreen_canvas = self.matrix.CreateFrameCanvas()
-        msg1 = CubeRgbText(matrix_id=0, text="BERLIN")
-        msg2 = CubeRgbText(matrix_id=1, text="BUDAPEST")
-        messages = [msg1, msg2]
-        # messages = [msg1]
-
-        while True:
+        while self._keep_running:
             offscreen_canvas.Clear()
-            for msg in messages:
+            for msg in self.messages:
                 msg.draw(canvas=offscreen_canvas)
             time.sleep(1)
             offscreen_canvas = self.matrix.SwapOnVSync(offscreen_canvas)
 
+    def stop(self):
+        self._keep_running = False
+        self._thread.join(timeout=0.5)
+
+
 class CubeRgbText:
-    PANEL_WIDTH = 64
-    X_MARGIN = 0
-    Y_TIME = 30
-    def __init__(self, matrix_id:int, text:str):
+    def __init__(self, matrix_id: int, text: str):
         self.text = text
         self.matrix_id = matrix_id
-        self.x = matrix_id * self.PANEL_WIDTH + self.X_MARGIN
+        self.x = matrix_id * PANEL_WIDTH + X_MARGIN
         self.start_time = time.time()
         self.font = graphics.Font()
         self.font.LoadFont(os.path.join(RGB_FONTS_DIR, "7x13.bdf"))
         self.textColor = graphics.Color(255, 255, 0)
 
     def draw(self, canvas):
-        graphics.DrawText(canvas, self.font, self.x, self.Y_TIME, self.textColor, self.text)
+        graphics.DrawText(canvas, self.font, self.x, Y_TEXT, self.textColor, self.text)
 
 
-# TODO: implement
-class CubeMasterLedMatrices:
-    # TODO: set this in a config file
-    NB_MATRICES = 2
-    PANEL_WIDTH = 64
-    X_MARGIN = 0
-    Y_TIME = 30
+class CubeRgbMatrixManager:
+    """A wrapper for CubeRgbTextDrawer, because CubeRgbTextDrawer is not run with `run`, but `process`.
+    I'd like to keep it coherent"""
     def __init__(self):
         self._drawer = CubeRgbTextDrawer()
-        self._thread = threading.Thread()
-        self._keep_running = False
 
-
-    def run(self):
-        self._thread = threading.Thread(target=self._drawing_loop)
-        self._keep_running = True
-        self._thread.start()
+    def run(self) -> bool:
+        return self._drawer.process()
 
     def stop(self):
-        self._keep_running = False
-        self._thread.join(timeout=0.5)
-
-    def _drawing_loop(self):
-        # NOTE: DO NOT copy the canvas in a CubeRgbText instance property.
-        # it seems to create new canvas instances, and makes the message disappear
-        start_time = time.time()
-        offscreen_canvas = self._drawer.matrix.CreateFrameCanvas()
-
-        while self._keep_running:
-            offscreen_canvas.Clear()
-            for matrix_id in range(self.NB_MATRICES):
-                # timestr = cube_utils.seconds_to_hhmmss_string(time.time() - start_time)
-                text = f"aaa{matrix_id}"
-                CubeRgbText(matrix_id=matrix_id, text=text).draw(canvas=offscreen_canvas)
-
-            time.sleep(1)
-            offscreen_canvas = self._drawer.matrix.SwapOnVSync(offscreen_canvas)
-
+        self._drawer.stop()
 
 
 
 # TODO: test display
 if __name__ == "__main__":
     import atexit
-    lm = CubeMasterLedMatrices()
+
+    lm = CubeRgbMatrixManager()
     atexit.register(lm.stop)
     lm.run()
