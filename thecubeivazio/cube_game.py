4c89a8b6337f60c68c9c15ec1c162ddabb9039b1
jwav <wavresky.julien@gmail.com>
2024-05-31 13:17:36 +0200

    the cubebox broadcasts at each status change now

--- thecubeivazio/cube_game.py
+++ thecubeivazio/cube_game.py
@@ -55,7 +55,7 @@ class CubeboxStatus:
     # TODO: set unknown state as the default
     def __init__(self, cube_id: CubeId = None, current_team_name: TeamName = None, start_timestamp: Seconds = None,
                  end_timestamp: Seconds = None, last_valid_rfid_line: cube_rfid.CubeRfidLine = None,
-                 state: CubeboxState = CubeboxState.STATE_UNKNOWN):
+                 state: CubeboxState = CubeboxState.STATE_WAITING_FOR_RESET):
         self.cube_id = cube_id
         self.current_team_name = current_team_name
         self.start_timestamp = start_timestamp
