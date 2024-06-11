f02f309d644733cce7f3b898ffa52483cea61061
jwav <wavresky.julien@gmail.com>
2024-06-11 19:26:17 +0200

    trying to set up autostart service

--- update_and_start_cubemaster.sh
+++ update_and_start_cubemaster.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 cd "${HOME}/thecube-ivazio" || exit
-source ../venv/bin/activate
+source venv/bin/activate
 bash ./update_version.sh
 cd "${HOME}/thecube-ivazio/thecubeivazio" || exit
 python3 cubeserver_master.py
