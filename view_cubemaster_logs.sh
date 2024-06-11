f4e55ab96547e2c0aef8c49721cc302699d19cef
jwav <wavresky.julien@gmail.com>
2024-06-11 19:22:27 +0200

    trying to set up autostart service

--- view_cubemaster_logs.sh
+++ view_cubemaster_logs.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-journalctl -u thecubeivazio.cubemaster.service -f --no-pager --output=short-iso-plain --no-hostname
+journalctl -u thecubeivazio.cubemaster.service -f --no-pager --no-hostname
