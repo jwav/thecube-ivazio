b091daf37e30a27af0d33008ae06a548e253c64f
jwav <wavresky.julien@gmail.com>
2024-06-22 00:20:39 +0200

    did the config tab. checking that the config is well received by servers when messaged MsgConfig

--- activate_venv.sh
+++ activate_venv.sh
@@ -3,5 +3,6 @@
 # Get the directory of the script
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
+echo "Activating virtual environment at ${SCRIPT_DIR}/venv/bin/activate"
 cd "$SCRIPT_DIR" || exit 1
 source "${SCRIPT_DIR}/venv/bin/activate"
