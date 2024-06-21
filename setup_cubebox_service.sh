6935acb8c494ba3fcae4e6eb8593633ff0da5bb3
jwav <wavresky.julien@gmail.com>
2024-06-22 00:47:07 +0200

    did the config tab. checking that the config is well received by servers when messaged MsgConfig

---
+++ setup_cubebox_service.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Define the source and destination paths
+SOURCE_PATH="${HOME}/thecube-ivazio/thecubeivazio.cubebox.service"
+DESTINATION_PATH="/etc/systemd/system/thecubeivazio.cubebox.service"
+
+# Check if the source file exists
+if [ ! -f "$SOURCE_PATH" ]; then
+    echo "Source file does not exist: $SOURCE_PATH"
+    exit 1
+fi
+
+# Copy the file to the destination, overwriting if it exists
+sudo install -m 644 "$SOURCE_PATH" "$DESTINATION_PATH" || exit 1
+
+
+# Reload systemd daemon to recognize the new service file
+sudo systemctl daemon-reload || exit 1
+
+# Enable the service to start on boot
+#sudo systemctl enable thecubeivazio.cubebox.service
+
+# Disable the service to ensure it does not start on boot
+sudo systemctl disable thecubeivazio.cubebox.service
+
+# Start the service immediately
+#sudo systemctl start thecubeivazio.cubebox.service
+
+echo "Service file copied and systemctl reloaded."
+
+
+# Output the status of the service
+sudo systemctl status thecubeivazio.cubebox.service
+
