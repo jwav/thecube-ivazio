76a3ed121db8c7e73043d11db03c6a2a0d1bcde0
jwav <wavresky.julien@gmail.com>
2024-06-22 00:42:40 +0200

    did the config tab. checking that the config is well received by servers when messaged MsgConfig

--- update_version.sh
+++ update_version.sh
@@ -5,6 +5,14 @@ RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m' # No Color
 
+# Functions for colored echo
+echo_red() {
+    echo -e "${RED}$1${NC}"
+}
+
+echo_green() {
+    echo -e "${GREEN}$1${NC}"
+}
 
 # Get the directory of the script
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
@@ -17,86 +25,87 @@ source "${SCRIPT_DIR}/venv/bin/activate"
   cd "$SCRIPT_DIR" || exit 1
   echo "Current working directory: $(pwd)"
 
-# TODO: by default, skip the APT update and pip install
-SKIP_APT=true
-SKIP_PIP_REQ=true
+  # TODO: by default, skip the APT update and pip install
+  SKIP_APT=true
+  SKIP_PIP_REQ=true
 
-for arg in "$@"
-do
-  if [ "$arg" == "--full-update" ]; then
-    SKIP_APT=false
-    SKIP_PIP_REQ=false
-    echo "Full update"
-    break
-  elif [ "$arg" == "--skip-apt" ]; then
-    SKIP_APT=true
-    echo "Skipping APT update and install"
-  elif [ "$arg" == "--skip-pip" ]; then
-    SKIP_PIP_REQ=true
-    echo "Skipping pip install"
-  fi
-done
+  for arg in "$@"
+  do
+    if [ "$arg" == "--full-update" ]; then
+      SKIP_APT=false
+      SKIP_PIP_REQ=false
+      echo "Full update"
+      break
+    elif [ "$arg" == "--skip-apt" ]; then
+      SKIP_APT=true
+      echo "Skipping APT update and install"
+    elif [ "$arg" == "--skip-pip" ]; then
+      SKIP_PIP_REQ=true
+      echo "Skipping pip install"
+    fi
+  done
 
-
-echo "Stashing local changes..."
-git stash
-echo "Pulling git..."
-git pull
-if [ $? -ne 0 ]; then
-  echo -e "${RED}ERROR: git pull failed${NC}"
-  exit 1
-else
-  echo -e "${GREEN}OK : git pull succeeded${NC}"
-fi
-
-
-if [ "$SKIP_APT" = false ]; then
-  echo "Updating APT and installing required packages.."
-  ./install_required_apt_packages.sh
+  echo "Stashing local changes..."
+  git stash
+  echo "Pulling git..."
+  git pull
   if [ $? -ne 0 ]; then
-      echo -e "${RED}ERROR: APT udpate and install failed${NC}"
-      exit 1
+    echo_red "ERROR: git pull failed"
+    exit 1
   else
-    echo -e "${GREEN}OK : The script succeeded${NC}"
+    echo_green "OK : git pull succeeded"
   fi
-fi
 
+  if [ "$SKIP_APT" = false ]; then
+    echo "Updating APT and installing required packages.."
+    ./install_required_apt_packages.sh
+    if [ $? -ne 0 ]; then
+      echo_red "ERROR: APT update and install failed"
+      exit 1
+    else
+      echo_green "OK : APT update and install succeeded"
+    fi
+  fi
 
-if [ "$SKIP_PIP_REQ" = false ]; then
-echo "pip install requirements..."
-pip install -r ./requirements.txt
-if [ $? -ne 0 ]; then
-  echo -e "${RED}ERROR: pip install requirements failed${NC}"
-  exit 1
-else
-  echo -e "${GREEN}OK : pip install requirements succeeded${NC}"
-fi
-fi
+  if [ "$SKIP_PIP_REQ" = false ]; then
+    echo "pip install requirements..."
+    pip install -r ./requirements.txt
+    if [ $? -ne 0 ]; then
+      echo_red "ERROR: pip install requirements failed"
+      exit 1
+    else
+      echo_green "OK : pip install requirements succeeded"
+    fi
+  fi
 
-echo "Installing the project package..."
-pip install .
-if [ $? -ne 0 ]; then
-  echo -e "${RED}ERROR: project package install failed${NC}"
-  exit 1
-else
-  echo -e "${GREEN}OK : project package install succeeded${NC}"
-fi
+  echo "Installing the project package..."
+  pip install .
+  if [ $? -ne 0 ]; then
+    echo_red "ERROR: project package install failed"
+    exit 1
+  else
+    echo_green "OK : project package install succeeded"
+  fi
 
-echo "Copying scripts..."
-# if the hostname is cubemaster, copy the cubemaster scripts, otherwise copy the cubebox scripts
-if [[ "$HOSTNAME" == "cubemaster" ]]; then
-  for file in *cubemaster*; do
-    cp "$file" ~/
-    chmod +x ~/"$file"
-  done
-elif [[ "$HOSTNAME" == *"cubebox"* ]]; then
-  for file in *cubebox*; do
-    cp "$file" ~/
-    chmod +x ~/"$file"
-  done
-fi
+  echo "Copying scripts..."
+  HOSTNAME=$(hostname)
+  if [[ "$HOSTNAME" == "cubemaster" ]]; then
+    echo "Hostname is cubemaster. Copying cubemaster scripts..."
+    for file in *cubemaster*; do
+      echo "Copying $file to home directory and making it executable."
+      cp "$file" ~/
+      chmod +x ~/"$file"
+    done
+  elif [[ "$HOSTNAME" == *"cubebox"* ]]; then
+    echo "Hostname contains cubebox. Copying cubebox scripts..."
+    for file in *cubebox*; do
+      echo "Copying $file to home directory and making it executable."
+      cp "$file" ~/
+      chmod +x ~/"$file"
+    done
+  else
+    echo "Hostname does not match cubemaster or cubebox patterns."
+  fi
 
-
-echo -e "${GREEN}Update OK: APT packages installed, git pulled, project package pip installed.${NC}"
-
+  echo_green "Update OK: APT packages installed, git pulled, project package pip installed."
 ) # End of subshell
