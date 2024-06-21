bc97fa013b99081bfca2c68cd81c1ea5f10d0b82
jwav <wavresky.julien@gmail.com>
2024-06-22 01:37:41 +0200

    enabling service start on boot in the setup_**_service.sh scripts

--- launch_cubebox.sh
+++ launch_cubebox.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-cd "${HOME}/thecube-ivazio" || exit
-source venv/bin/activate || exit
 cd "${HOME}/thecube-ivazio/thecubeivazio" || exit
+source ../venv/bin/activate || exit
 python3 cubeserver_cubebox.py || exit
