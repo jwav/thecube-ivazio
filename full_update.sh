12f765155a34b75ba4716d16a2a4eaa340a4653e
jwav <wavresky.julien@gmail.com>
2024-06-26 20:06:16 +0200

    copied cubemaster scripts to cubebox scripts. lets see if it works

--- full_update.sh
+++ full_update.sh
@@ -1,14 +1,21 @@
-# Get the directory of the script
-#SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+#!/usr/bin/env bash
 
-# if the hostname contains "cube", use this defined directory
+# Stop the services
+sudo systemctl stop thecubeivazio.cubebox.service
+sudo systemctl stop thecubeivazio.cubemaster.service
+
+# Determine the directory based on the hostname
 if [[ $(hostname) == *"cube"* ]]; then
   THECUBE_DIR="${HOME}/thecube-ivazio"
 else
   THECUBE_DIR="/mnt/shared/thecube-ivazio"
 fi
 
-#cd "$SCRIPT_DIR" || exit 1
-#source "${SCRIPT_DIR}/update_version.sh --full-update"
-#cd "${THECUBE_DIR}" || exit 1
+# Ensure the update_version.sh script exists
+if [[ ! -f "${THECUBE_DIR}/update_version.sh" ]]; then
+  echo "Error: ${THECUBE_DIR}/update_version.sh not found"
+  exit 1
+fi
+
+# Run the update_version.sh script with the --full-update argument
 bash "${THECUBE_DIR}/update_version.sh" --full-update
