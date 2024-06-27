d2a617318d21806bb3626787fab23643830cf39f
jwav <wavresky.julien@gmail.com>
2024-06-27 18:32:49 +0200

    i'm having a lot of trouble with the rfid keyboardlistener which i didnt have before. let's call it a day.

--- start_cubemaster_service.sh
+++ start_cubemaster_service.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
+sudo systemctl stop thecubeivazio.cubemaster.service
 sudo systemctl start thecubeivazio.cubemaster.service
 sudo systemctl status thecubeivazio.cubemaster.service
