76d18a11083f73c308ce764dc1dc83fc61c0a141
jwav <wavresky.julien@gmail.com>
2024-06-27 18:40:09 +0200

    i'm having a lot of trouble with the rfid keyboardlistener which i didnt have before. let's call it a day.

--- launch_cubemaster.sh
+++ launch_cubemaster.sh
@@ -4,5 +4,5 @@ set -x
 echo "Launching CubeMaster service..."
 cd "${HOME}/thecube-ivazio/thecubeivazio" || { echo "Failed to change directory"; exit 1; }
 source ../venv/bin/activate || { echo "Failed to activate virtual environment"; exit 1; }
-python3 cubeserver_master.py || { echo "Failed to start CubeMaster"; exit 1; }
+python3 ./cubeserver_master.py || { echo "Failed to start CubeMaster"; exit 1; }
 echo "CubeMaster service started."
