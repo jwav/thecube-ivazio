7fcfc8f4b5c090936bc1eaa770d86bd27d02be01
jwav <wavresky.julien@gmail.com>
2024-06-27 19:14:53 +0200

    it now seems that the autostart service works now

--- launch_cubebox.sh
+++ launch_cubebox.sh
@@ -4,5 +4,5 @@
 echo "Launching CubeBox service..."
 cd "${HOME}/thecube-ivazio/thecubeivazio" || { echo "Failed to change directory"; exit 1; }
 source ../venv/bin/activate || { echo "Failed to activate virtual environment"; exit 1; }
-python3 ./cubeserver_master.py || { echo "Failed to start CubeBox"; exit 1; }
+python3 ./cubeserver_cubebox.py || { echo "Failed to start CubeBox"; exit 1; }
 echo "CubeBox service started."
