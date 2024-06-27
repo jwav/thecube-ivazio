e1eef2e944ebcfc08248b186bcbc392c6d925d24
jwav <wavresky.julien@gmail.com>
2024-06-27 14:09:39 +0200

    i'm having a lot of trouble with the rfid keyboardlistener which i didnt have before. let's call it a day.

--- update_and_start_cubebox.sh
+++ update_and_start_cubebox.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-cd "${HOME}/thecube-ivazio/thecubeivazio" || exit
-source ../venv/bin/activate
-bash ../update_version.sh
-python3 cubeserver_master.py
+cd "${HOME}/thecube-ivazio" || exit
+bash ./update_version.sh
+bash ./launch_cubebox.sh
