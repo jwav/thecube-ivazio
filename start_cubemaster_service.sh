5a69523dcf7560a36c1d0cf416c82b22c30b5279
jwav <wavresky.julien@gmail.com>
2024-08-01 18:21:59 +0200

    changed the service and launch files so that they accept the --skip-update arg. not tested yet.

--- start_cubemaster_service.sh
+++ start_cubemaster_service.sh
@@ -1,8 +1,19 @@
 #!/usr/bin/env bash
 
-# hide mouse cursor
-/usr/bin/unclutter -idle 1 -root &
+ARG=""
+
+for arg in "$@"; do
+  case $arg in
+    --skip-update)
+      ARG="--skip-update"
+      shift
+      ;;
+    *)
+      shift
+      ;;
+  esac
+done
 
 sudo systemctl stop thecubeivazio.cubemaster.service
-sudo systemctl start thecubeivazio.cubemaster.service
+sudo systemctl start thecubeivazio.cubemaster.service $ARG
 #sudo systemctl status thecubeivazio.cubemaster.service
--- update_and_launch_cubemaster.sh
+++ update_and_launch_cubemaster.sh
@@ -1,6 +1,27 @@
 #!/usr/bin/env bash
 
+SKIP_UPDATE=false
+
+for arg in "$@"; do
+  case $arg in
+    --skip-update)
+      SKIP_UPDATE=true
+      shift
+      ;;
+    *)
+      shift
+      ;;
+  esac
+done
+
+# hide mouse cursor
+/usr/bin/unclutter -idle 1 -root &
+
 cd "${HOME}/thecube-ivazio" || exit
 source venv/bin/activate
-bash ./update_version.sh
+
+if [ "$SKIP_UPDATE" = false ]; then
+  bash ./update_version.sh
+fi
+
 bash ./launch_cubemaster.sh
