1676cbb864806178cb96234567f2f2f942e32ad2
jwav <wavresky.julien@gmail.com>
2024-08-07 17:21:26 +0200

    trying to setup cubebox3 from scratch...

--- install_pip_requirements.sh
+++ install_pip_requirements.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # Get the directory of the script
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
@@ -5,10 +7,17 @@ cd "$SCRIPT_DIR" || exit 1
 source "${SCRIPT_DIR}/venv/bin/activate"
 echo "Activating virtual environment at ${SCRIPT_DIR}/venv/bin/activate"
 
-echo "pip install requirements..."
-pip install -r ./requirements.txt
+if [[ "$1" == "--full-reinstall" ]]; then
+  echo "Full reinstall requested. Forcing reinstallation of all requirements without cache..."
+  yes | pip install --no-cache-dir --force-reinstall -r ./requirements.txt
+else
+  echo "pip install requirements..."
+  yes | pip install -r ./requirements.txt
+fi
+
 if [ $? -ne 0 ]; then
   echo "ERROR: pip install requirements failed"
+  deactivate
   exit 1
 else
   echo "OK : pip install requirements succeeded"
