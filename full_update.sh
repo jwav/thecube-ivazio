0c3efae1638388d470db5d6f46c994be9d03e5e7
jwav <wavresky.julien@gmail.com>
2024-08-08 15:12:29 +0200

    massive refactor: using generic *thecube* scripts instead of *cubebox* and *cubemaster* stripts.
    trying to setup cubebox3 from scratch...

--- full_update.sh
+++ full_update.sh
@@ -1,21 +1,16 @@
 #!/usr/bin/env bash
 
-# Stop the services
-sudo systemctl stop thecubeivazio.cubebox.service
-sudo systemctl stop thecubeivazio.cubemaster.service
-
-# Determine the directory based on the hostname
-if [[ $(hostname) == *"cube"* ]]; then
-  THECUBE_DIR="${HOME}/thecube-ivazio"
-else
-  THECUBE_DIR="/mnt/shared/thecube-ivazio"
-fi
-
-# Ensure the update_thecube.sh script exists
-if [[ ! -f "${THECUBE_DIR}/update_thecube.sh" ]]; then
-  echo "Error: ${THECUBE_DIR}/update_thecube.sh not found"
+source "/home/ivazio/thecube-ivazio/thecube_common_defines.sh" || source "/mnt/shared/thecube-ivazio/thecube_common_defines.sh" || {
+  echo "ERROR: Could not load thecube_common_defines.sh"
   exit 1
-fi
+}
+
+
+# Stop the services
+stop_thecube_service
 
 # Run the update_thecube.sh script with the --full-update argument
-bash "${THECUBE_DIR}/update_thecube.sh" --full-update
+bash "${THECUBE_DIR}/update_thecube.sh" --full-update || {
+  echo_red "ERROR: Could not run the update_thecube.sh script"
+  exit 1
+}
